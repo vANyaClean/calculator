2e2b04d5c55749d92e0554d4e30f164fd45d3584
vANyaClean <ivanhulihan@gmail.com>
2020-06-05 13:53:52 +0000

    added dom

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
-document.querySelectorAll('.digits button')
-   .forEach(button => button.addEventListerer('click', addDigits))
-   function addDigits(ev){
-   display.value  += ev.target.innerText;
-   }
+const display = document.querySelector(".display");
+document
+  .querySelectorAll(".digits button")
+  .forEach((button) => button.addEventListener("click", digitsPressed));
+function digitsPressed(ev) {
+  display.value += ev.target.innerText;
+}
+
+document
+  .querySelectorAll(".opers button")
+  .forEach((button) => button.addEventListener("click", opersPressed));
+function opersPressed(ev) {
+  display.value += ev.target.innerText;
+}
+document.querySelector(".eq").addEventListener("click", equals);
+function equals() {
+  if (display.value.match("/0")) {
+    alert("You can not do this");
+  } else {
+    display.value = eval(display.value).toFixed(2);
+    }
+}
+document.querySelector(".erase").addEventListener("click", erase);
+function erase() {
+  display.value = "";
+}
+document.querySelector(".eraselast").addEventListener("click", eraselast);
+function eraselast() {
+  display.value = display.value.slice(0, -1);
+}
